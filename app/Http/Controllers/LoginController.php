522a0c90afaf7e2731563151f6d778b5f53575f2
Veasna Chhay <your_github_email@example.com>
2026-02-11 15:18:28 +0700

    Remove redirect to login page

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,10 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function showLoginForm()
-    {
-        return view('auth.login');
-    }
+
 
     public function login(Request $request)
     {
